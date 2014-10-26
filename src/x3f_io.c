51da568099ceff07abc6911daed13fa77a402d3c
Roland Karlsson <roland.karlsson@erlang-solutions.com>
2014-10-26 16:39:31 +0100

    Cosmetic

--- src/x3f_io.c
+++ src/x3f_io.c
@@ -2580,7 +2580,7 @@ static void x3f_setup_camf_entries(x3f_camf_t *CAMF)
 {
   uint8_t *p = (uint8_t *)CAMF->decoded_data;
   uint8_t *end = p + CAMF->decoded_data_size;
-  camf_entry_t *table = NULL;
+  camf_entry_t *entry = NULL;
   int i;
 
   printf("SETUP CAMF ENTRIES\n");
@@ -2620,58 +2620,58 @@ static void x3f_setup_camf_entries(x3f_camf_t *CAMF)
     }
 
     /* TODO: lots of realloc - may be inefficient */
-    table = (camf_entry_t *)realloc(table, (i+1)*sizeof(camf_entry_t));
+    entry = (camf_entry_t *)realloc(entry, (i+1)*sizeof(camf_entry_t));
 
     /* Pointer */
-    table[i].entry = p;
+    entry[i].entry = p;
 
     /* Header */
-    table[i].id = *p4++;
-    table[i].version = *p4++;
-    table[i].entry_size = *p4++;
-    table[i].name_offset = *p4++;
-    table[i].value_offset = *p4++;
+    entry[i].id = *p4++;
+    entry[i].version = *p4++;
+    entry[i].entry_size = *p4++;
+    entry[i].name_offset = *p4++;
+    entry[i].value_offset = *p4++;
 
     /* Compute adresses and sizes */
-    table[i].name_address = p + table[i].name_offset;
-    table[i].value_address = p + table[i].value_offset;
-    table[i].name_size = table[i].value_offset - table[i].name_offset;
-    table[i].value_size = table[i].entry_size - table[i].value_offset;
+    entry[i].name_address = p + entry[i].name_offset;
+    entry[i].value_address = p + entry[i].value_offset;
+    entry[i].name_size = entry[i].value_offset - entry[i].name_offset;
+    entry[i].value_size = entry[i].entry_size - entry[i].value_offset;
 
-    table[i].text_size = 0;
-    table[i].text = NULL;
-    table[i].property_num = 0;
-    table[i].property_name = NULL;
-    table[i].property_value = NULL;
-    table[i].matrix_type = 0;
-    table[i].matrix_dim = 0;
-    table[i].matrix_data_off = 0;
-    table[i].matrix_data = NULL;
-    table[i].matrix_dim_entry = NULL;
+    entry[i].text_size = 0;
+    entry[i].text = NULL;
+    entry[i].property_num = 0;
+    entry[i].property_name = NULL;
+    entry[i].property_value = NULL;
+    entry[i].matrix_type = 0;
+    entry[i].matrix_dim = 0;
+    entry[i].matrix_data_off = 0;
+    entry[i].matrix_data = NULL;
+    entry[i].matrix_dim_entry = NULL;
 
-    table[i].matrix.as_float = NULL;
-    table[i].matrix.as_int = NULL;
-    table[i].matrix.as_uint = NULL;
+    entry[i].matrix.as_float = NULL;
+    entry[i].matrix.as_int = NULL;
+    entry[i].matrix.as_uint = NULL;
 
-    switch (table[i].id) {
+    switch (entry[i].id) {
     case X3F_CMbP:
-      x3f_setup_camf_property_entry(&table[i]);
+      x3f_setup_camf_property_entry(&entry[i]);
       break;
     case X3F_CMbT:
-      x3f_setup_camf_text_entry(&table[i]);
+      x3f_setup_camf_text_entry(&entry[i]);
       break;
     case X3F_CMbM:
-      x3f_setup_camf_matrix_entry(&table[i]);
+      x3f_setup_camf_matrix_entry(&entry[i]);
       break;
     }
 
-    p += table[i].entry_size;
+    p += entry[i].entry_size;
   }
 
  stop:
 
   CAMF->entry_table.size = i;
-  CAMF->entry_table.element = table;
+  CAMF->entry_table.element = entry;
 
   printf("SETUP CAMF ENTRIES (READY) Found %d entries\n", i);
 }
