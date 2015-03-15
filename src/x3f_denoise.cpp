5055e30b9c30323e0494de99eb3263fa3a6d57ec
vagrant <vagrant@ubuntu-14.04-amd64-vbox>
2015-03-15 17:41:22 +0000

    starting to split up denoise

--- src/x3f_denoise.cpp
+++ src/x3f_denoise.cpp
@@ -6,6 +6,7 @@
 #include <opencv2/imgproc.hpp>
 
 #include "x3f_denoise.h"
+#include "x3f_denoise_utils.h"
 #include "x3f_io.h"
 #include <algorithm> //for std::sort for the median filter
 
@@ -13,111 +14,13 @@
 
 using namespace cv;
 
-typedef void (*conv_t)(x3f_area16_t *image);
-
 typedef struct {
   double h;
   conv_t BMT_to_YUV;
   conv_t YUV_to_BMT;
 } denoise_desc_t;
 
-static const int32_t O_UV = 32768; // To avoid clipping negative values in U,V
 
-// Matrix used to convert BMT to YUV:
-//  0    0    1
-//  2    0   -2
-//  1   -2    1
-static void BMT_to_YUV_YisT(x3f_area16_t *image)
-{
-  for (uint32_t row=0; row < image->rows; row++)
-    for (uint32_t col=0; col < image->columns; col++) {
-      uint16_t *p = &image->data[row*image->row_stride + col*image->channels];
-
-      int32_t B = (int32_t)p[0];
-      int32_t M = (int32_t)p[1];
-      int32_t T = (int32_t)p[2];
-
-      int32_t Y =               +T;
-      int32_t U =   +2*B      -2*T;
-      int32_t V =     +B -2*M   +T;
-
-      p[0] = saturate_cast<uint16_t>(Y);
-      p[1] = saturate_cast<uint16_t>(U + O_UV);
-      p[2] = saturate_cast<uint16_t>(V + O_UV);
-    }
-}
-
-// Matrix used to convert YUV to BMT:
-//  1    1/2  0
-//  1    1/4 -1/2
-//  1    0    0
-static void YUV_to_BMT_YisT(x3f_area16_t *image)
-{
-  for (uint32_t row=0; row < image->rows; row++)
-    for (uint32_t col=0; col < image->columns; col++) {
-      uint16_t *p = &image->data[row*image->row_stride + col*image->channels];
-
-      int32_t Y = (int32_t)p[0];
-      int32_t U = (int32_t)p[1] - O_UV;
-      int32_t V = (int32_t)p[2] - O_UV;
-
-      int32_t B = ( +2*Y   +U      ) / 2;
-      int32_t M = ( +4*Y   +U -2*V ) / 4;
-      int32_t T = (   +Y           )    ;
-
-      p[0] = saturate_cast<uint16_t>(B);
-      p[1] = saturate_cast<uint16_t>(M);
-      p[2] = saturate_cast<uint16_t>(T);
-    }
-}
-
-// Matrix used to convert BMT to YUV:
-//  1/3  1/3  1/3
-//  2    0   -2
-//  1   -2    1
-static void BMT_to_YUV_STD(x3f_area16_t *image)
-{
-  for (uint32_t row=0; row < image->rows; row++)
-    for (uint32_t col=0; col < image->columns; col++) {
-      uint16_t *p = &image->data[row*image->row_stride + col*image->channels];
-
-      int32_t B = (int32_t)p[0];
-      int32_t M = (int32_t)p[1];
-      int32_t T = (int32_t)p[2];
-
-      int32_t Y = (   +B   +M   +T ) / 3;
-      int32_t U =   +2*B      -2*T;
-      int32_t V =     +B -2*M   +T;
-
-      p[0] = saturate_cast<uint16_t>(Y);
-      p[1] = saturate_cast<uint16_t>(U + O_UV);
-      p[2] = saturate_cast<uint16_t>(V + O_UV);
-    }
-}
-
-// Matrix used to convert YUV to BMT:
-//  1    1/4  1/6
-//  1    0   -1/3
-//  1   -1/4  1/6
-static void YUV_to_BMT_STD(x3f_area16_t *image)
-{
-  for (uint32_t row=0; row < image->rows; row++)
-    for (uint32_t col=0; col < image->columns; col++) {
-      uint16_t *p = &image->data[row*image->row_stride + col*image->channels];
-
-      int32_t Y = (int32_t)p[0];
-      int32_t U = (int32_t)p[1] - O_UV;
-      int32_t V = (int32_t)p[2] - O_UV;
-
-      int32_t B = ( +12*Y +3*U +2*V ) / 12;
-      int32_t M = (  +3*Y        -V ) /  3;
-      int32_t T = ( +12*Y -3*U +2*V ) / 12;
-
-      p[0] = saturate_cast<uint16_t>(B);
-      p[1] = saturate_cast<uint16_t>(M);
-      p[2] = saturate_cast<uint16_t>(T);
-    }
-}
 
 static void denoise(const Mat& in, Mat& out, double h)
 {
@@ -283,55 +186,7 @@ static void denoise_aniso(const uint32_t& in_rows, const uint32_t& in_columns, f
   delete [] h;
 }
 
-static float* convert_to_float_image(x3f_area16_t *image)
-{
-  uint32_t x, y, i;
-  const uint32_t num_rows = image->rows;
-  const uint32_t num_cols = image->columns;
-  const uint32_t jump = image->channels;
-  float* outImage = new float[num_cols * num_rows * jump];
-  float* out_ptr = outImage;
-  uint16_t* in_ptr;
-  
-  for (y = 0; y < num_rows; y++)
-  {
-    for (x = 0, in_ptr = (image->data + y * image->row_stride); x < num_cols; x++)
-    {
-      for (i = 0; i < jump; i++, out_ptr++, in_ptr++)
-      {
-        *out_ptr = (float(*in_ptr)/65535.0f);
-      }
-    }
-  }
-  return outImage;
-}
 
-static void convert_from_float_image(x3f_area16_t *image, float* in_float_image)
-{
-  uint32_t x, y, i;
-  const uint32_t num_rows = image->rows;
-  const uint32_t num_cols = image->columns;
-  const uint32_t jump = image->channels;
-  float* in_float_ptr = in_float_image;
-  uint16_t* in_ptr;
-  float new_val;
-  for (y = 0; y < num_rows; y++)
-  {
-    for (x = 0, in_ptr = (image->data + y * image->row_stride); x < num_cols; x++)
-    {
-      //skipping the first channel, leaving that behind
-      in_ptr++;
-      in_float_ptr++;
-      for (i = 1; i < jump; i++, in_ptr++, in_float_ptr++)
-      {
-        new_val = *in_float_ptr;
-        if (new_val < 0) new_val = 0;
-        if (new_val > 1.0f) new_val = 1.0f; //clamping
-        *in_ptr = new_val * 65535.0f;
-      }
-    }
-  }
-}
 #endif //NLM
 
 static const denoise_desc_t denoise_types[] = {
@@ -373,6 +228,8 @@ void x3f_denoise(x3f_area16_t *image, x3f_denoise_type_t type)
 // NOTE: active has to be a subaera of image, i.e. they have to share
 //       the same data area.
 // NOTE: image and active will be destructively modified in place.
+// NOTE: while not specifically a denoising function, denoising is applied
+// to the quattro image as a part of conversion.
 void x3f_expand_quattro(x3f_area16_t *image, x3f_area16_t *active,
 			x3f_area16_t *qtop,
 			x3f_area16_t *expanded, x3f_area16_t *active_exp)
