272405f723888133575cd4a51ee0d987a0aa9b5e
Erik Karlsson <erik.r.karlsson@gmail.com>
2015-03-19 22:53:12 +0100

    Locking to commit in the OpenCV repo

--- install_opencv.sh
+++ install_opencv.sh
@@ -24,22 +24,22 @@ OCV_SRC=$SRC/opencv
 OCV_BLD=$SRC/$SYS/opencv_build
 OCV_LIB=$LIB/opencv
 
+OCV_URL=https://github.com/erikrk/opencv.git
+OCV_HASH=82c54104d6901e03027240cd9c6866f6b2509d0a
+
 if [ -e $OCV_SRC ] ; then
-    echo Pull opencv
+    echo Fetch opencv
     cd $OCV_SRC || exit 1
-    git checkout denoising-16bit-master || exit 1
-    git pull || exit 1
-    cd $ROOT || exit 1
+    git fetch || exit 1
 else
     echo Clone opencv
     mkdir -p $SRC || exit 1
-    git clone https://github.com/erikrk/opencv.git $OCV_SRC || exit 1
+    git clone $OCV_URL $OCV_SRC || exit 1
     cd $OCV_SRC || exit 1
-    git checkout denoising-16bit-master || exit 1
-    cd $ROOT || exit 1
 fi
 
 echo Build Opencv
+git checkout $OCV_HASH
 mkdir -p $OCV_BLD || exit 1
 mkdir -p $OCV_LIB || exit 1
 cd $OCV_BLD || exit 1
@@ -47,7 +47,5 @@ cmake -D CMAKE_INSTALL_PREFIX=$OCV_LIB -D CMAKE_BUILD_TYPE=RELEASE \
       -D WITH_TBB=ON -D BUILD_TBB=ON -D BUILD_TIFF=ON -D BUILD_SHARED_LIBS=OFF \
        $OCV_SRC || exit 1
 make -j $CORES install || exit 1
-cd $ROOT || exit 1
-
 touch $OCV_LIB/.success || exit 1
 echo Successfully installed OpenCV in $OCV_LIB
