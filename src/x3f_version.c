9546d5533b5dd8b38523574ba2df202ead930509
Roland Karlsson <roland.karlsson@proxel.com>
2016-09-11 19:46:58 +0200

    Print version

---
+++ src/x3f_version.c
@@ -0,0 +1,10 @@
+/* X3F_VERSION.C
+ *
+ * Contains the version of X3F Tools
+ *
+ * Copyright 2016 - Roland and Erik Karlsson
+ * BSD-style - see doc/copyright.txt
+ *
+ */
+
+char *version = VERSION;
